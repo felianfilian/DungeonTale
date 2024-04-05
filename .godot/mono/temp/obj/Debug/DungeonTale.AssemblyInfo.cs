560281d588af31ea2235ef072fe07729ae40fb9d
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-05 05:09:02 +0200

    setup main scene

--- .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55aeec7535737a2a99beeec36e0bf615cefb3012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31705a5b71861dcd3a5dc11bff830a621847187f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
