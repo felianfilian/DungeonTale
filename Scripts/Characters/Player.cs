746a8141f604e445ed52edc9283ed1736e10e7f3
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-06 08:09:48 +0200

    export attributes

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [Export] private AnimationPlayer ainmPlayerNode;
+    [Export] private Sprite3D sprite3D;
+
     private Vector2 direction = new(); // default (0, 0)
+
+    public override void _Ready()
+    {
+        GD.Print(ainmPlayerNode.Name);
+        GD.Print(sprite3D.Name);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
         Velocity *= 5;
-        
+
         MoveAndSlide();
     }
 
