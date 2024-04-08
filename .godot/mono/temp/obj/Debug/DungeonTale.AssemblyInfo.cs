effe421402a5252f6bc49f41ad524b5beb69319f
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-08 21:38:33 +0200

    state machine

--- .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3adfe6b80be7c63aa61230ae3a27479b079a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125e6488362cea1f3064b6b7ff917924df863b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
