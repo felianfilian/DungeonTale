f89b43d7584a4d144b4aca8db24a73c9490d6106
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-08 22:11:26 +0200

    state machine update

--- .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125e6488362cea1f3064b6b7ff917924df863b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+effe421402a5252f6bc49f41ad524b5beb69319f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scripts/Characters/Player/PlayerMoveState.cs
+++ Scripts/Characters/Player/PlayerMoveState.cs
@@ -3,8 +3,17 @@ using System;
 
 public partial class PlayerMoveState : Node
 {
+    private Player player;
+
     public override void _Ready() {
-        
+        player = GetOwner<Player>();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if(player.direction == Vector2.Zero) {
+            player.stateMachineNode.SwitchState<PlayerIdleState>();
+        }
     }
 
     public override void _Notification(int what)
