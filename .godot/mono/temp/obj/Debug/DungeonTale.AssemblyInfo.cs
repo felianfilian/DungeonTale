493d2fde069285264d399803a1e67e049044e15b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-15 04:31:35 +0200

    camera setup

--- .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/DungeonTale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b9af0a4192ba39509710b12e84bcab359c9529")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b64169fd2ce748ab4dd03dcfa429281b1d75c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonTale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
